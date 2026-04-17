7fe4614891e38b86eab0bd7ecc1fb6a90b07119b
charanaruna05 <charanaruna051@gmail.com>
2026-04-17 14:27:03 +0530

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAaFSPKG6PedRVfca8BvP3YvV3-6E-80iI",
-  authDomain: "trinetr-pro-90ba3.firebaseapp.com",
-  projectId: "trinetr-pro-90ba3",
-  storageBucket: "trinetr-pro-90ba3.appspot.com",
-  messagingSenderId: "90389254119",
-  appId: "1:90389254119:web:f971812839994998463870",
-  databaseURL: "https://trinetr-pro-90ba3-default-rtdb.firebaseio.com"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  databaseURL: import.meta.env.VITE_DATABASE_URL,
 };
 
 const app = initializeApp(firebaseConfig);

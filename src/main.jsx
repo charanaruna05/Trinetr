92271c831cf622a134051d3b78c438ea8a12b384
charanaruna05 <charanaruna051@gmail.com>
2026-04-20 15:47:08 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,3 +3,11 @@ import { createRoot } from 'react-dom/client';
 import { AppProvider } from './context/AppContext.jsx';
 import App from './App.jsx';
 import './styles/global.css';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <AppProvider>
+      <App />
+    </AppProvider>
+  </StrictMode>
+);
